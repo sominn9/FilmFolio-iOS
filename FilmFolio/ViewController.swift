d473838bf74a848c8968462ab26b498d45bd6e24
sominn9 <tlsthals3415@naver.com>
2023-05-09 22:45:13 +0900

    Initial Commit

---
+++ FilmFolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmFolio
+//
+//  Created by 신소민 on 2023/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
