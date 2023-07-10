aacfe5562c0126656130920e62deeac59ddbd145
sominn9 <tlsthals3415@naver.com>
2023-07-10 23:57:36 +0900

    feat: Search 화면 구현을 위한 VC, ViewModel 생성

---
+++ FilmFolio/ViewControllers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  FilmFolio
+//
+//  Created by 신소민 on 2023/07/10.
+//
+
+import RxCocoa
+import RxSwift
+import SnapKit
+import UIKit
+
+final class SearchViewController: UIViewController {
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ FilmFolio/ViewModels/SearchViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewModel.swift
+//  FilmFolio
+//
+//  Created by 신소민 on 2023/07/10.
+//
+
+import Foundation
+import RxSwift
+
+struct SearchViewModel {
+    
+    // MARK: Input & Output
+    
+    struct Input {
+
+    }
+    
+    struct Output {
+
+    }
+    
+}
