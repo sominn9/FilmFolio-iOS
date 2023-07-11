a9b4280d55653cbac397fc138d0650fe1847b04d
sominn9 <tlsthals3415@naver.com>
2023-07-12 02:09:21 +0900

    feat: 탭 바 구현

--- FilmFolio/ViewControllers/SearchViewController.swift
+++ FilmFolio/ViewControllers/SearchViewController.swift
@@ -12,10 +12,65 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
+    // MARK: Constants
+    
+    struct Metric {
+        static let collectionViewInset = 16.0
+        static let collectionViewSpacing = 8.0
+    }
+    
+    
+    // MARK: Properties
+    
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = String(localized: "Search Movie")
+        searchBar.searchBarStyle = .minimal
+        return searchBar
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewCompositionalLayout.grid(
+            spacing: Metric.collectionViewSpacing,
+            inset: Metric.collectionViewInset
+        )
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsVerticalScrollIndicator = false
+        return collectionView
+    }()
+    
+    
     // MARK: View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    
+    // MARK: Methods
+    
+    private func configure() {
+        view.backgroundColor = .systemBackground
+        navigationItem.title = String(localized: "Search")
+        layout()
+    }
+    
+    private func layout() {
+        view.addSubview(searchBar)
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+        }
+        
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.bottom.equalTo(view.snp.bottom)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+        }
     }
     
 }
---
+++ FilmFolio/ViewControllers/TabBarController.swift
@@ -0,0 +1,73 @@
+//
+//  TabBarController.swift
+//  FilmFolio
+//
+//  Created by 신소민 on 2023/07/12.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    // MARK: Initializing
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    
+    // MARK: Methods
+    
+    private func configure() {
+        self.tabBar.isTranslucent = true
+        setViewControllers()
+        setTabBarItems()
+    }
+    
+    private func setViewControllers() {
+        let viewControllers = [
+            UINavigationController(rootViewController: HomeViewController()),
+            UINavigationController(rootViewController: SearchViewController()),
+            UINavigationController(),
+            UINavigationController()
+        ]
+        self.setViewControllers(viewControllers, animated: true)
+    }
+    
+    private func setTabBarItems() {
+        guard let viewControllers else { return }
+        viewControllers[0].tabBarItem = UITabBarItem(
+            title: String(localized: "Home"),
+            image: UIImage(systemName: "house.fill"),
+            tag: 0
+        )
+        viewControllers[1].tabBarItem = UITabBarItem(
+            title: String(localized: "Search"),
+            image: UIImage(systemName: "magnifyingglass"),
+            tag: 1
+        )
+        viewControllers[2].tabBarItem = UITabBarItem(
+            title: String(localized: "Upcoming"),
+            image: UIImage(systemName: "play.rectangle"),
+            tag: 2
+        )
+        viewControllers[3].tabBarItem = UITabBarItem(
+            title: "My",
+            image: UIImage(systemName: "suit.heart.fill"),
+            tag: 3
+        )
+    }
+    
+}
