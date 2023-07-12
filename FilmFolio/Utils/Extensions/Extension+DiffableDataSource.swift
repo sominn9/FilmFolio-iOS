6a43cba1cdb84ec3cd68e91a1bf02b0a6d116d4b
sominn9 <tlsthals3415@naver.com>
2023-07-12 17:29:22 +0900

    delete: cellProvider 관련 함수 삭제

--- FilmFolio/Utils/Extensions/Extension+DiffableDataSource.swift
+++ FilmFolio/Utils/Extensions/Extension+DiffableDataSource.swift
@@ -7,17 +7,6 @@
 
 import UIKit
 
-extension UICollectionViewDiffableDataSource {
-    
-    static func cellProvider<Cell>(using registration: UICollectionView.CellRegistration<Cell, ItemIdentifierType>) -> CellProvider {
-        
-        return { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell? in
-            
-            return collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: itemIdentifier)
-        }
-    }
-}
-
 extension UICollectionView.SupplementaryRegistration where Supplementary == TitleView {
     
     static func registration(elementKind: String, title: String) -> Self {
