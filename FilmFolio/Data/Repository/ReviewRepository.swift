74181f1c010722a4a55fbe11222dea9cafd1bb5e
sominn9 <tlsthals3415@naver.com>
2023-08-30 01:44:33 +0900

    fix: NSPredicate 에러 해결 (%@ -> %d)

--- FilmFolio/Data/Repository/ReviewRepository.swift
+++ FilmFolio/Data/Repository/ReviewRepository.swift
@@ -12,6 +12,7 @@ protocol ReviewRepository {
     func save(_ review: Review) async throws
     func fetch(_ id: Int, _ media: Media) async throws -> Review?
     func fetch(offset: Int, count: Int) async throws -> [Review]
+    func update(_ review: Review) async throws
     func delete(_ review: Review) async throws
 }
 
@@ -39,9 +40,10 @@ struct DefaultReviewRepository: ReviewRepository {
     
     func fetch(_ id: Int, _ media: Media) async throws -> Review? {
         let request = ReviewEntity.fetchRequest()
-        let predicate1 = NSPredicate(format: "id == %@", id)
-        let predicate2 = NSPredicate(format: "media == %@", media.rawValue)
+        let predicate1 = NSPredicate(format: "id = %d", id)
+        let predicate2 = NSPredicate(format: "media = %@", media.rawValue)
         request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1, predicate2])
+        
         let result = try await dataSource.fetch(request)
         return result.first?.toDomain()
     }
@@ -50,14 +52,28 @@ struct DefaultReviewRepository: ReviewRepository {
         let request = ReviewEntity.fetchRequest()
         request.fetchOffset = offset
         request.fetchLimit = count
+        
         return try await dataSource.fetch(request).map { $0.toDomain() }
+    }
+    
+    func update(_ review: Review) async throws {
+        let request = ReviewEntity.fetchRequest()
+        let predicate1 = NSPredicate(format: "id = %d", review.id)
+        let predicate2 = NSPredicate(format: "media = %@", review.media.rawValue)
+        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1, predicate2])
+        
+        try await dataSource.update(request) { entity in
+            entity.title = review.title
+            entity.content = review.content
+        }
     }
     
     func delete(_ review: Review) async throws {
         let request = ReviewEntity.fetchRequest()
-        let predicate1 = NSPredicate(format: "id == %@", review.id)
-        let predicate2 = NSPredicate(format: "media == %@", review.media.rawValue)
+        let predicate1 = NSPredicate(format: "id = %d", review.id)
+        let predicate2 = NSPredicate(format: "media = %@", review.media.rawValue)
         request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate1, predicate2])
+        
         try await dataSource.delete(request)
     }
     
